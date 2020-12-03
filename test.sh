ba74b172abe2cd4b7bc93f85ce5a8345290a8983
KheliaNibaruta <73180785+KheliaNibaruta@users.noreply.github.com>
2020-12-03 16:58:38 +0100

    Delete test.sh

--- test.sh
+++
@@ -1,5 +0,0 @@
-HTTPCODE=echo $(`curl -s -o /dev/null -w "%{http_code}" https://salty-journey-63595.herokuapp.com/` | tr -d ' ')
-if [ "$HTTPCODE" -ne 200 ];then
-	echo "heroku app not responding, failing deploy"
-	exit 1
-fi
