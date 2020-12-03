5143caa6b968caaff7fdfff3a547d90b51adb192
khelia <knibaruta@gmail.com>
2020-12-03 19:37:09 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-HTTPCODE=`curl -s -o /dev/null -w "%{http_code}" https://salty-journey-63595.herokuapp.com/`
+HTTPCODE=echo $(`curl -s -o /dev/null -w "%{http_code}" https://salty-journey-63595.herokuapp.com/` | tr -d ' ')
 if [ "$HTTPCODE" -ne 200 ];then
 	echo "heroku app not responding, failing deploy"
 	exit 1
